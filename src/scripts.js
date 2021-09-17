d4e9120f327fa7d95df0ff0dd4b2cee64cb0c8a0
luiscetta <filipe.cetta@gmail.com>
2021-09-16 23:53:26 -0300

    first commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,52 @@
 // lib para gerar um número aleatório de 0 a 10.
 let numeroSecreto = parseInt(Math.random() * 11);
-
 console.log("o numero secreto é " + numeroSecreto);
 
+let tentativas = 3;
+const elementoResultado = document.getElementById("resultado");
+
+// pode esconder o form todo
+let hideForm = false;
+let hideTryAgainButton = true;
+
+function toggleTryAgainButton() {
+  document.getElementById("again-btn").hidden = hideTryAgainButton;
+}
+
+function toggleForm() {
+  hideForm = !hideForm;
+
+  document.getElementById("form-1").hidden = hideForm;
+}
+
+function fillResult(text) {
+  document.getElementById("resultado").innerHTML = text;
+}
+
+
 function Chutar() {
-  let elementoResultado = document.getElementById("resultado");
 
-  // pegar apenas o valor inserido no input; parseInt - pega apenas numeros inteiros.
-  let chute = parseInt(document.getElementById("valor").value);
-  console.log(chute);
+    // pegar apenas o valor inserido no input; parseInt - pega apenas numeros inteiros.
+    let chute = parseInt(document.getElementById("valor").value);
+    console.log(chute);
 
-  if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = "Acertô, mizeravi!";
-  } else if (chute > 10 || chute < 0) {
-    elementoResultado.innerHTML = "Digite um número de 0 a 10!";
-  } else {
-    elementoResultado.innerHTML = "Errrrrou! Tente novamente! ";
-  }
+    if (chute == numeroSecreto) {
+      fillResult("Acertô, mizeravi!")
+    } else if (chute > 10 || chute < 0) {
+      fillResult("Digite um número de 0 a 10!")
+    } else {
+      tentativas -= 1;
+      tentativas === 0 ?
+        TryAgain(`Tentativas esgotadas! O número era ${numeroSecreto}.`)
+        :
+        fillResult(`Errrrrou! Tente novamente. ${tentativas} tentativas restantes.`)
+    }
+}
+
+function TryAgain(message) {
+  toggleForm();
+  toggleTryAgainButton();
+  fillResult(message);
 }
 
 // numero de tentativas
